0b2cdb2f0ff8f19202a420c8c307957ad60a7b6d
Maciej Motak <maciej.motak@gmail.com>
2017-03-13 17:45:01 +0100

    Added or example

--- sampleapp/src/main/java/pl/com/mmotak/sample/viewModels/LoginExampleViewModel.java
+++ sampleapp/src/main/java/pl/com/mmotak/sample/viewModels/LoginExampleViewModel.java
@@ -21,7 +21,7 @@ public class LoginExampleViewModel {
             "^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-]+)*@"
                     + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
-    private static final String LOGIN_PATTERN = "[A-Za-z0-9_.]+";
+    private static final String LOGIN_PATTERN = "^[A-Za-z_][A-Za-z0-9_.]{2,29}$";
 
     private Rule<String> orRule = new Rule<String>() {
         @Override
@@ -41,7 +41,7 @@ public class LoginExampleViewModel {
             new RuleCommand.Builder<String>()
                     .withRule(new RegexRule("[\\S]+", "Whitespace characters not allowed")) // THE ORDER IS IMPORTANT!
                     .withRule(new MinimumLengthRule(3, "Three or more characters"))
-                    //.withRule(new MaximumLengthRule(12, "No more then twelve characters"))
+                    .withRule(new MaximumLengthRule(30, "No more then 30 characters"))
                     .withRule(orRule)
                     .build());
 
